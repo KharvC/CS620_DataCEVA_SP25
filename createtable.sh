701b50c228063ccb26d21c5e6c8926380e9c3738
KharvC <kbchoksi@wisc.edu>
2025-02-21 15:26:33 -0600

    Created a shell file to run all commands to set up docker and create the table with data

---
+++ createtable.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Installs sodapy and mysql connector for Python
+pip install sodapy
+pip install mysql-connector-python
+
+# Starts the MySQL service
+docker-compose up -d
+
+# Makes sure that enough time has been given to MySQL sevice to be set up
+sleep 7
+
+# Runs the SQL queries to create the table
+docker exec -i liquor_sales_IA_db mysql -u root -pcs620ibdc1234 <<EOF
+USE IowaLiquorSales;
+SOURCE /docker-entrypoint-initdb.d/create.sql;
+EOF
+
+# Runs a python script to insert data from Iowa liquor sales website into a table in the database
+python DataLoader.py
