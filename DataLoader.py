3c5dd449485ee4d9ae6199660d669a5c7ec688e7
KharvC <kbchoksi@wisc.edu>
2025-02-21 09:35:43 -0600

    Updated DataLoader.py to parse through all rows of data by calling the API.

--- DataLoader.py
+++ DataLoader.py
@@ -2,6 +2,19 @@ from sodapy import Socrata
 
 client = Socrata("data.iowa.gov", None)
 
-results = client.get("cc6f-sgik", limit=1)
+check = True
+ofst = 0
 
-print(results)
+'''
+    The while loop calls the API till all entries have been parsed through.
+    There is 50,000 limit for the API so we change the offset to get all the data.
+    results is a list of dictionaries
+'''
+while (check):
+    results = client.get("cc6f-sgik", limit=50000, offset = 50000*ofst)
+    print(ofst)
+    if (len(results) == 0):
+        check = False
+    ofst += 1
+    #MySQL code for insetion here
+    
